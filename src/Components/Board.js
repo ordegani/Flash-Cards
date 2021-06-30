8d414ed20f3ba0d23f256aec57f49b88a3b49f6d
ordegani <oradeganii@gmail.com>
2021-06-30 18:52:57 +0300

    changes

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,29 +1,44 @@
-import React from "react";
+import {useState} from "react";
 import Card from "./Card";
 
 export default function Board() {
-  const Data = [
-    { country: "Israel", capital: "Jerusalem" },
-    { country: "England", capital: "London" },
-    { country: "Russia", capital: "moscow" },
-    { country: "Egypt", capital: "Cairo" },
-    { country: "India", capital: "New Delhi" },
-  ];
-  localStorage.setItem('data', Data);
-  
+    const [Data, setData] = useState([
+        { country: "Israel", capital: "Jerusalem" },
+        { country: "England", capital: "London" },
+        { country: "Russia", capital: "moscow" },
+        { country: "Egypt", capital: "Cairo" },
+        { country: "India", capital: "New Delhi" },
+      ]);
+      localStorage.setItem('data', Data);
   console.log(Data);
+
+//if Data was an array without objets in it:
+//   const Delete = (card) => {
+//     setData()
+//     console.log(Data[1].country);}
+
+// TOCOMPLETE:
+    const Delete = (index) => {
+        Data.splice(index, 1);
+        // renderData();
+        console.log(Data);
+      };
+
   return (
     <div>
       <div className="countryContainer">
         {Data.map((card, index) => (
-          <Card className="cardCountry" country={card.country}
+          <Card className="card" country={card.country}
         //    />
     //     ))}
     //   </div>
     //   <div className="capitalContainer">
     //   {Data.map((card, index) => (
         // <Card 
-        className="cardCapital" capital={card.capital} />
+        // className="cardCapital" 
+        capital={card.capital} 
+        onClick ={Delete}
+        buttonText="DELETE"/>
       ))}
       </div>
     </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Board from "./Board";
 
-export default function Card({country, capital}) {
+export default function Card({country, capital, onClick, buttonText}) {
   return (
     <div className="card">
       <h1>{country}</h1>
       <h1>{capital}</h1>
+      <button
+        className="deleteBtn"
+        onClick={() => onClick({ country, capital})}
+        type="Submit"
+      >
+        {buttonText}
+      </button>
     </div>
   );
 }
